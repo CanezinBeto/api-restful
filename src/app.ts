451f2fb58456412ef86ff2be2c265295da8b7e60
CanezinBeto <albertocanezin@gmail.com>
2022-06-19 14:34:54 -0300

    Integrando o Express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('On')
+import express from 'express'
+import config from 'config'
+
+const app = express()
+
+// JSON Middleware
+app.use(express.json())
+
+// App port
+const port = config.get<number>('port')
+
+app.listen(port, async () => {
+  console.log(`Aplicação está funcionando na porta: ${port}`)
+})
