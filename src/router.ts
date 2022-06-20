1e75bc1b7624bc4746f0d60c841f1acd967b6ad9
CanezinBeto <albertocanezin@gmail.com>
2022-06-20 20:57:02 -0300

    Middleware de validação de erros

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,10 @@
 import { Router, Request, Response } from 'express'
 import { createMovie } from './controllers/movieController'
+import { validate } from './middleware/handleValidation'
 const router = Router()
 
 export default router
   .get('/test', (req: Request, res: Response) => {
     res.status(200).send('Api Working')
   })
-  .post('/movie', createMovie)
+  .post('/movie', validate, createMovie)
