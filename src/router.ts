06f8725816682a187590db79ea46cab5cd549ad8
CanezinBeto <albertocanezin@gmail.com>
2022-06-20 21:12:51 -0300

    Validação de filmes

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from 'express'
 import { createMovie } from './controllers/movieController'
 import { validate } from './middleware/handleValidation'
+import { movieCreateValidation } from './middleware/movieValidation'
 const router = Router()
 
 export default router
   .get('/test', (req: Request, res: Response) => {
     res.status(200).send('Api Working')
   })
-  .post('/movie', validate, createMovie)
+  .post('/movie', movieCreateValidation(), validate, createMovie)
