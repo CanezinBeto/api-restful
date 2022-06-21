eb1fdf79a37ea52b4856a3f1d450378caeb650a9
CanezinBeto <albertocanezin@gmail.com>
2022-06-21 18:47:25 -0300

    Resgatando filme por ID

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express'
-import { createMovie } from './controllers/movieController'
+import { createMovie, findMovieById } from './controllers/movieController'
 import { validate } from './middleware/handleValidation'
 import { movieCreateValidation } from './middleware/movieValidation'
 const router = Router()
@@ -9,3 +9,4 @@ export default router
     res.status(200).send('Api Working')
   })
   .post('/movie', movieCreateValidation(), validate, createMovie)
+  .get('/movie/:id', findMovieById)
